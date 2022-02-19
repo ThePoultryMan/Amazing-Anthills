db445b51832b08b9fd4f35daf2a844e3a585f725
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-02-18 20:24:53 -0500

    Initializer class setup

--- src/main/java/thepoultryman/anthills/AmazingAnthills.java
+++ src/main/java/thepoultryman/anthills/AmazingAnthills.java
@@ -5,11 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AmazingAnthills implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "anthills";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing [inset mod name here]");
+		LOGGER.info("Initializing super silly anthills");
 	}
 }
